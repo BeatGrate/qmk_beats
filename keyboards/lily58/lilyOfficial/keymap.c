ab08b26439a35ba380aa808b8571a638c1eba958
BeatGrate <ezzard@Bread>
2023-04-23 10:39:39 -0700

    Moving wpm up

--- keyboards/lily58/lilyOfficial/keymap.c
+++ keyboards/lily58/lilyOfficial/keymap.c
@@ -278,14 +278,14 @@ static void print_logo_narrow(void) {
     /* wpm counter */
     uint8_t n = get_current_wpm();
     char    wpm_str[4];
-    oled_set_cursor(0, 8);
+    oled_set_cursor(0, 9);
     wpm_str[3] = '\0';
     wpm_str[2] = '0' + n % 10;
     wpm_str[1] = '0' + (n /= 10) % 10;
     wpm_str[0] = '0' + n / 10;
     oled_write(wpm_str, false);
 
-    oled_set_cursor(0, 15);
+    oled_set_cursor(0, 8);
     oled_write(" wpm", false);
 }
 
