3fe5693e52bcd6e215a96198efb4597c15f4d232
BeatGrate <gregoryicbondoc@gmail.com>
2023-04-22 17:46:43 -0700

    More path change.

--- runner.sh
+++ runner.sh
@@ -5,6 +5,6 @@ if [ -z "$1" ]
 then
   echo "Please enter a keymap directory name, no quotes after the ./runner.sh" 
 else
-  rm -rf /mnt/c/Users/grego/qmk_firmware/keyboards/lily58/keymaps/$1
-  cp -r ./keyboards/lily58/$1 /mnt/c/Users/grego/qmk_firmware/keyboards/lily58/keymaps/$1
+  rm -rf /mnt/c/Users/Gregory/qmk_firmware/keyboards/lily58/keymaps/$1
+  cp -r ./keyboards/lily58/$1 /mnt/c/Users/Gregory/qmk_firmware/keyboards/lily58/keymaps/$1
 fi
